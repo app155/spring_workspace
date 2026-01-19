776111164cc72141e1cde054346b20bf0a5ccbe2
1k10 <77734986+app155@users.noreply.github.com>
2026-01-19 14:31:51 +0900

    20260119
    20260119

---
+++ BoardWeb/src/main/java/com/globalin/biz/common/AroundAdvice.java
@@ -0,0 +1,32 @@
+package com.globalin.biz.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
+
+@Service
+@Aspect
+public class AroundAdvice {
+	@Pointcut("execution(* com.globalin.biz..*Impl.*(..))")
+	public void allPointcut() {	}
+
+	@Pointcut("execution(* com.globalin.biz..*Impl.get*(..))")
+	public void getPointcut() {	}
+	
+	@Around("allPointcut()")
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+		//System.out.println("[BEFORE]: 비즈니스 메소드 수행 전 처리할 내용");
+		String method = pjp.getSignature().getName();
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		Object returnObj = pjp.proceed();
+		stopWatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간=" + stopWatch.getTotalTimeMillis() + "(ms)");
+		//System.out.println("[AFTER]: 비즈니스 메소드 수행 후 처리할 내용");
+		
+		return returnObj;
+	}
+}
--- BoardWeb/src/main/java/com/globalin/biz/common/Log4jAdvice.java
+++ BoardWeb/src/main/java/com/globalin/biz/common/Log4jAdvice.java
@@ -1,6 +1,20 @@
 package com.globalin.biz.common;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+//@Service
+//@Aspect
 public class Log4jAdvice {
+	//@Pointcut("execution(* com.globalin.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	//@Pointcut("execution(* com.globalin.biz..*Impl.get*(..))")
+	public void getPointcut() {}
+	
+	//@Before("allPointcut()")
 	public void printLogging() {
 		System.out.println("[공통 로그-Log4j] 비즈니스 로직 수행 전 동작");
 	}
